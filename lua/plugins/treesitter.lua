038d5be316b5e23ade5668649f347fc55a79231f
Bill Kerr <bill@tailwindapp.com>
2025-03-28 16:59:17 -0500

    Initial config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function() 
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+    ensure_installed = { "lua", "javascript", "go", "typescript" },
+    highlight = { enable = true },
+    indent = { enable = true },
+})
+
+end }
---
+++ lua/remaps.lua
@@ -0,0 +1,10 @@
+local builtin = require("telescope.builtin")
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>pf", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>pv", builtin.find_files, {})
+vim.keymap.set("n", "<leader>pe", vim.cmd.Ex, {})
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
