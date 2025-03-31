fb1999c5597523ef45add40639b127db9b479380
Bill Kerr <bill@tailwindapp.com>
2025-03-31 13:07:05 -0500

    Organize imports on save

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,10 +1,19 @@
--- format on save
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
 	callback = function(args)
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			buffer = args.buf,
 			callback = function()
+				-- organize imports
+				vim.lsp.buf.code_action({
+					context = { only = { "source.organizeImports.biome" } },
+					apply = true,
+				})
+
+				-- source.organizeImports is non-blocking, so we wait a reasonable amount of time here.
+				vim.wait(100)
+
+				-- format
 				vim.lsp.buf.format { async = false, id = args.data.client_id }
 			end
 		})
