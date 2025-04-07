dc0dbc0048de7afc929f0fb394c2c5e247cf9093
Bill Kerr <bill@tailwindapp.com>
2025-04-07 06:00:12 -0500

    Add a few options and remaps

--- lua/options.lua
+++ lua/options.lua
@@ -9,3 +9,18 @@ vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.colorcolumn = "100"
+vim.g.have_nerd_font = true
+vim.opt.cursorline = true
+vim.opt.confirm = true
+
+-- Keeps cursor at least 10 lines from bottom of editor
+vim.opt.scrolloff = 10
+
+-- Case insensitive search unless \C or one or more capital letters are in search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
--- lua/remaps.lua
+++ lua/remaps.lua
@@ -4,13 +4,13 @@ local harpoon_mark = require("harpoon.mark")
 local harpoon_ui = require("harpoon.ui")
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pf", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>pv", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.git_status, {})
 vim.keymap.set("n", "<leader>pd", builtin.diagnostics, {})
 vim.keymap.set("n", "<leader>po", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 vim.keymap.set("n", "<leader>pe", vim.cmd.Ex, {})
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
@@ -26,3 +26,14 @@ end)
 vim.keymap.set("n", "<leader>af", harpoon_mark.add_file, {})
 vim.keymap.set("n", "<leader>h", harpoon_ui.toggle_quick_menu, {})
 vim.keymap.set("n", "<leader>ef", vim.diagnostic.open_float, {})
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<leader>/", function()
+	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
+end, { desc = "[/] Fuzzily search in current buffer" })
